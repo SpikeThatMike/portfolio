1359863b4f0622c241453daa772832333efb5318
SpikeThatMike <michaelcrimes04@gmail.com>
2022-04-27 19:46:23 +0100

    jquery smooth scroll

--- src/scripts/scroll.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    console.log("loaded")
-    window.scrollBy({ 
-        top: 500, // could be negative value
-        left: 0, 
-        behavior: 'smooth' 
-    });
-      
-}
