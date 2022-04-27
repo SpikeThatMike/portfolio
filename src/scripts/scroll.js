eba6a40a2ab5bd57e808138ed08796be385e8f10
SpikeThatMike <michaelcrimes04@gmail.com>
2022-04-27 19:36:30 +0100

    smooth scrolling

---
+++ src/scripts/scroll.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    console.log("loaded")
+    window.scrollBy({ 
+        top: 500, // could be negative value
+        left: 0, 
+        behavior: 'smooth' 
+    });
+      
+}
